d2b884831f6c8b85e8b2c9ff40e20d79b90f2347
Junjie Jin <jjjjohnsonjin@gmail.com>
2017-09-03 19:31:53 +0800

    Achieve basic function

--- designs.js
+++ designs.js
@@ -1,10 +1,34 @@
 // Select color input
-// Select size input
+let colorPicker = $('#colorPicker');
+// // Select size input
+let inputHeight = $( "input:first" );
+let inputWidth = $( "input:first" ).next();
+
+let canvas = $('#pixel_canvas');
+// console.log('input_width', input_width);
 
 // When size is submitted by the user, call makeGrid()
+$( "#sizePicker" ).submit(function makeGrid(event) {
+  // let input_height = $('#input_height').val();
+  input_height = inputHeight.val();
+  input_width = inputWidth.val();
+  // console.log('input_height:',input_height);
+  // console.log('input_width:',input_width);
+  // console.log(colorPicker.val());
 
-function makeGrid() {
+  $("#pixel_canvas tr").remove(); 
 
-// Your code goes here!
+	// canvas.attr('bgcolor', colorPicker.val());
+  for ( let i=0; i < inputHeight.val(); i++){
+  	canvas.append('<tr></tr>');
+  	for ( let j=0; j < inputWidth.val(); j++){
+  		$('tr:last-of-type').append('<td> </td>');
+  	}
+  }
+  event.preventDefault();
 
-}
+});
+
+$("#pixel_canvas").on("click", "td", function() {
+	$(this).attr("bgcolor", colorPicker.val());
+});
